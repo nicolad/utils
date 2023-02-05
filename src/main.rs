390a9bb16a7baf0647b39d738f14ff1a5a56bb5e
vadim.nicolai-EXT <vadim.nicolai-EXT@vitrifi.net>
2023-02-05 07:52:29 +0200

    Small improvement.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use mini_redis::{client, Result};
+use reqwest::Client;
+use serde::Deserialize;
+use serde_json::from_str;
+use tokio;
+
+#[derive(Deserialize, Debug)]
+struct SearchResult {
+    items: Vec<Repository>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Repository {
+    name: String,
+    stargazers_count: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rt = tokio::runtime::Runtime::new().unwrap();
+
+    rt.block_on(async {
+        let client = Client::new();
+
+        let response = client
+            .get("https://api.github.com/search/repositories?q=cumulus+in:dependencies")
+            .send()
+            .await
+            .unwrap();
+
+        let text = response.text().await.unwrap();
+
+        let result: SearchResult = from_str(&text).unwrap();
+
+        let repositories = result
+            .items
+            .iter()
+            .map(|repo| (repo.stargazers_count, repo.name.clone()))
+            .collect::<Vec<(i32, String)>>();
+
+        let sorted_repositories = repositories
+            .into_iter()
+            .filter(|(stars, _)| *stars > 0)
+            .collect::<Vec<(i32, String)>>();
+
+        for (stars, repo_name) in sorted_repositories {
+            println!("{} stars: {}", stars, repo_name);
+        }
+    });
 }
